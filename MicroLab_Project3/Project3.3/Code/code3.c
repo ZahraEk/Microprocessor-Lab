6f481077fa3b2b1c1cca6221af29848d303f7e12
ZahraEk <49494014+ZahraEk@users.noreply.github.com>
2021-01-04 04:31:39 +0330

    project3.question 3 added!

--- MicroLab_Project3/Project3.3/Code/code3.c
+++ MicroLab_Project3/Project3.3/Code/code3.c
@@ -29,29 +29,79 @@ Data Stack size         : 256
 #define RS 0             //RS=0
 #define EN 1             //EN=1
 
+int ov_counter;
+int i = 0;
+unsigned int start_edge, end_edge;
+unsigned int clocks,period_out;
+char frequency[10];
+
 void lcd_comm(char);
 void lcd_data(char);
 void lcd_init(void);
 void lcd_clear(void);
 		
+interrupt [TIM1_OVF] void timer1_ovf_isr(void){   // Timer1 overflow ISR
+
+     ++ov_counter;
+}
+
+interrupt [TIM1_CAPT] void timer1_capt_isr(void){  //Timer1 input capture ISR
+
+      //combine two 8bit capture register into the 16 bit count
+      end_edge = 256 * ICR1H + ICR1L + ov_counter * 65536;
+      clocks =(unsigned long)end_edge + ((unsigned long)ov_counter *65536) - (unsigned long)start_edge;  
+      
+      period_out = (clocks/750) ;  //output ms     
+      
+      ov_counter = 0;   //clear overflow
+      start_edge = end_edge;   //save end time to use as start edge
+      
+      sprintf(frequency,"%d Hz", period_out); 
+      lcd_init();    //initialize LCD      
+
+     //adding data
+      for(i=0 ; i<6 ; i++){    
+           
+           lcd_data(frequency[i]);
+            
+      }
+
+}
+
+void main(void)
+{
+
+TCCR1A = 0x00;
+TCCR1B = 0xC2; //enable input capture mode / prescaler 8
+
+TIMSK = 0x24;  //unmask timer1 overflow and capture interrupts
+
+#asm("sei")    // Enable global interrupts
+
+while (1)
+      {
+       ;   //do nothing
+
+      }
+}
 
 void lcd_comm(char x){
 
-    PORTD = x;
-    PORTC &= ~(1<<RS);  //RS=0 command reg.
-    PORTC |= 1<<EN;     //Enable Pulse
+    PORTB = x;
+    PORTA &= ~(1<<RS);  //RS=0 command reg.
+    PORTA |= 1<<EN;     //Enable Pulse
     delay_ms(30);
-    PORTC &= ~(1<<EN);  //Disable Pulse
+    PORTA &= ~(1<<EN);  //Disable Pulse
     
 }
 
 void lcd_data(char x){
 
-    PORTD = x;
-    PORTC |= 1<<RS;    //RS=1 Data reg.
-    PORTC |= 1<<EN;    //Enable Pulse
+    PORTB = x;
+    PORTA |= 1<<RS;    //RS=1 Data reg.
+    PORTA |= 1<<EN;    //Enable Pulse
     delay_ms(30);
-    PORTC &= ~(1<<EN); //Disable Pulse
+    PORTA &= ~(1<<EN); //Disable Pulse
     
 }
 
@@ -63,8 +113,8 @@ void lcd_clear()
 
 void lcd_init(void){
 
-    DDRD = 0xFF;      //PORTD initialization
-    DDRC = 0x03;      //PORTC initialization
+    DDRA = 0x03;      //PORTA initialization   
+    DDRB = 0xFF;      //PORTB initialization 
     
     lcd_comm(0x38);   //initialization of 16X2 LCD in 8bit mode 
     lcd_comm(0x06);   //auto Increment cursor
